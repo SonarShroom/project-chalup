0402aa5761bc04ff0e7e2e746ac2df222f006b79
gui <guilhermessecoortiz@gmail.com>
2021-06-25 16:41:19 +0100

    Scripts for audio Implementation

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    #region Singleton
+    public static AudioManager instance;
+    private void Awake()
+    {
+       if(instance == null)
+        instance = this;
+
+
+            foreach (Sound s in sounds)
+        {
+           s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+    #endregion
+
+    public Sound[] sounds;
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, Sound => Sound.name == name);
+       
+      
+        if (s == null) 
+        {
+            Debug.Log( "\"" + name + "\"" + " could not be found");
+            return;
+        }
+        s.source.Play();
+    }
+}
---
+++ Assets/Scripts/Audio/CharacterAudio.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+public class CharacterAudio : MonoBehaviour
+{
+
+    public Sound[] sounds;
+
+    private void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    //plays the sound by name
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, Sound => Sound.name == name);
+
+
+        if (s == null)
+        {
+            Debug.Log("\"" + name + "\"" + " could not be found");
+            return;
+        }
+        s.source.Play();
+    }
+    //plays the sound at point overlapping with any current sound
+    public void SpawnAudio(string name, Vector3 point)
+    {
+        Sound s = Array.Find(sounds, Sound => Sound.name == name);
+
+
+        if (s == null)
+        {
+            Debug.Log("\"" + name + "\"" + " could not be found");
+            return;
+        }
+        AudioSource.PlayClipAtPoint(s.source.clip, point);
+    }
+}
