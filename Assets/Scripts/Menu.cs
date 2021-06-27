52acf0ffff51afa99d5a3268143d4ca4f135296a
gui <guilhermessecoortiz@gmail.com>
2021-06-27 19:02:32 +0100

    changes

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
 
@@ -17,14 +18,22 @@ public class Menu : MonoBehaviour
     public Text Arrowcounterdisplay;
     public int ArrowCounter;
     public int MaxArrowAmount;
+
+    //Display
+    public Slider sensesslide;
+    public Text sensestext;
+    public Toggle exploration;
+
+
     private void Update()
     {
         Arrowcounterdisplay.text = "";
         Arrowcounterdisplay.text = ArrowCounter.ToString() + "/" + MaxArrowAmount.ToString();
 
-
+        Preferencesholder.instance.MouseSensitivity = sensesslide.value;
+        Preferencesholder.instance.isExplorationMode = exploration.isOn;
+        sensestext.text = sensesslide.value.ToString();
     }
-
 
 
 
@@ -48,4 +57,14 @@ public class Menu : MonoBehaviour
         if (ArrowCounter < 0)
             ArrowCounter = 0;
     }
+    //Main menu Methods
+    public void StartGame(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+    public void OpenOptions(GameObject optionsmenu)
+    {
+        optionsmenu.SetActive(!optionsmenu.activeSelf);
+    }
+
 }
---
+++ Assets/Scripts/Preferencesholder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Preferencesholder : MonoBehaviour
+{
+    #region Singleton
+    public static Preferencesholder instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+
+    public float MouseSensitivity;
+    public bool isExplorationMode;
+
+   
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    
+}
